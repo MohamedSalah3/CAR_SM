d159d7c84a6bf7a8a3d607a8010084fc5c660942
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2021-03-09 10:37:14 +0200

    Test

---
+++ car_sm.c
@@ -0,0 +1,34 @@
+﻿/*
+ * car_sm.c
+ *
+ * Created: 19/02/2020 07:30:03 ص
+ *  Author: mo
+ */ 
+
+#include "car_sm.h"
+extern uint16_t Distance;
+ERROR_STATUS Car_SM_Init(void)
+{
+
+Steering_Init();
+Us_Init();
+
+}
+
+
+ERROR_STATUS Car_SM_Update(void)
+{
+Us_Trigger();
+Us_GetDistance(&Distance);
+
+if(Distance <= 30)
+{
+	
+	Steering_SteerCar(CAR_RIGHT,50);
+}else
+{
+	Steering_SteerCar(CAR_FORWARD,50);
+}
+}	
+
+
